13134c8b600cd4dfa500c3dee2ca4c1d99598955
Sigurd Fosseng <sigurd@fosseng.net>
2017-04-18 12:48:27 +0200

    remove .only

--- mvn-artifact-download/test/test.js
+++ mvn-artifact-download/test/test.js
@@ -49,7 +49,7 @@ describe('mvn-artifact-url', function () {
     let dl = download('org.apache.commons:commons-lang3:3.4')
     expect(dl).to.eventually.be.rejected.and.notify(done)
   })
-  it.only('should download latest snapshot version of artifact', function (done) {
+  it('should download latest snapshot version of artifact', function (done) {
     let metadataXml = '<metadata><groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId><version>3.4-SNAPSHOT</version><versioning><snapshot><timestamp>1</timestamp><buildNumber>23</buildNumber></snapshot><lastUpdated>20120607154257</lastUpdated><snapshotVersions><snapshotVersion><extension>jar</extension><value>1.0-20120607.154257-1339076577</value><updated>20120607154257</updated></snapshotVersion></snapshotVersions></versioning></metadata>';
     nock('https://repo1.maven.org/maven2/')
     .get('/org/apache/commons/commons-lang3/3.4-SNAPSHOT/maven-metadata.xml')

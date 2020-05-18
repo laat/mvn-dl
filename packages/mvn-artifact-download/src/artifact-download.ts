c34b9d130ae0ea6e24aa4bea276380a6a99c8ffe
Sigurd Fosseng <sigurd@fosseng.net>
2020-05-18 15:50:48 +0200

    add fetch options

--- packages/mvn-artifact-download/src/artifact-download.ts
+++ packages/mvn-artifact-download/src/artifact-download.ts
@@ -1,9 +1,23 @@
 import fs from 'fs';
-import path from 'path';
-import parseName from 'mvn-artifact-name-parser';
+import { Agent } from 'http';
 import getFilename from 'mvn-artifact-filename';
+import parseName from 'mvn-artifact-name-parser';
 import artifactUrl from 'mvn-artifact-url';
 import fetch from 'node-fetch';
+import path from 'path';
+
+export interface FetchOptions {
+  /**
+   * http.Agent instance, allows custom proxy, certificate etc.
+   * @default null
+   */
+  agent?: Agent | ((parsedUrl: URL) => Agent);
+  /**
+   * req/res timeout in ms, it resets on redirect. 0 to disable (OS limit applies)
+   * @default 0
+   */
+  timeout?: number;
+}
 
 function pipeToFile(body: NodeJS.ReadableStream, destFile: string) {
   return new Promise((resolve, reject) => {
@@ -24,18 +38,19 @@ export default (async function download(
   artifactName: string,
   destination?: string,
   repository?: string,
-  filename?: string
+  filename?: string,
+  fetchOptions: FetchOptions = {}
 ) {
   destination = destination || process.cwd();
   const artifact = parseName(artifactName);
 
-  const url = await artifactUrl(artifact, repository);
+  const url = await artifactUrl(artifact, repository, fetchOptions);
 
   const destFile = path.join(
     destination || process.cwd(),
     filename || getFilename(artifact)
   );
-  const response = await fetch(url);
+  const response = await fetch(url, fetchOptions);
   if (response.status !== 200) {
     throw new Error(`Unable to fetch ${url}. Status ${response.status}`);
   }
--- packages/mvn-artifact-url/src/artifact-url.ts
+++ packages/mvn-artifact-url/src/artifact-url.ts
@@ -1,6 +1,7 @@
-import path from 'path';
+import { Agent } from 'http';
 import filename from 'mvn-artifact-filename';
 import fetch from 'node-fetch';
+import path from 'path';
 import parseXmlString from './parseXmlString';
 
 export interface Artifact {
@@ -11,6 +12,19 @@ export interface Artifact {
   classifier?: string;
   isSnapShot?: boolean;
   snapShotVersion?: string;
+}
+
+export interface FetchOptions {
+  /**
+   * http.Agent instance, allows custom proxy, certificate etc.
+   * @default null
+   */
+  agent?: Agent | ((parsedUrl: URL) => Agent);
+  /**
+   * req/res timeout in ms, it resets on redirect. 0 to disable (OS limit applies)
+   * @default 0
+   */
+  timeout?: number;
 }
 
 function groupPath(artifact: Artifact): string {
@@ -25,9 +39,16 @@ function artifactPath(artifact: Artifact): string {
   return path.join(groupPath(artifact), filename(artifact));
 }
 
-async function latestSnapShotVersion(artifact: Artifact, basepath: string) {
+async function latestSnapShotVersion(
+  artifact: Artifact,
+  basepath: string,
+  fetchOptions: FetchOptions = {}
+) {
   const metadataUrl = basepath + groupPath(artifact) + '/maven-metadata.xml';
-  const response = await fetch(metadataUrl);
+  const response = await fetch(metadataUrl, {
+    agent: fetchOptions.agent,
+    timeout: fetchOptions.timeout
+  });
   if (response.status !== 200) {
     throw new Error(
       `Unable to fetch ${metadataUrl}. Status ${response.status}`
@@ -42,11 +63,12 @@ async function latestSnapShotVersion(artifact: Artifact, basepath: string) {
 
 export default (async function artifactUrl(
   artifact: Artifact,
-  basePath?: string
+  basePath?: string,
+  fetchOptions: FetchOptions = {}
 ) {
   const prefix = basePath || 'https://repo1.maven.org/maven2/';
   if (artifact.isSnapShot) {
-    const snapShotVersion = await latestSnapShotVersion(artifact, prefix);
+    const snapShotVersion = await latestSnapShotVersion(artifact, prefix, fetchOptions);
     return prefix + artifactPath({ snapShotVersion, ...artifact });
   } else {
     return prefix + artifactPath(artifact);
